5f606fb3a972d13b36ca16ce970028970ad66326
sjtg <chipilej@gmail.com>
2019-12-27 05:31:36 +0200

    removed the admin in the navbar

--- signups.php
+++ signups.php
@@ -25,9 +25,6 @@
     <li class="nav-item">
       <a class="nav-link" href="index.php">Home</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="admin.php">Admin</a>
-    </li>
 
     <!-- Dropdown -->
     <li class="nav-item dropdown">
